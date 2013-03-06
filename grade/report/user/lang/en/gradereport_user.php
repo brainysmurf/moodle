c35a47daab54d717670e2a5ffb24d3b0179599a0
Adam Morris <amorris@mistermorris.com>
2013-03-06 17:00:36 +0800

    grade report language is messed up, fixed for our context

--- grade/report/user/lang/en/gradereport_user.php
+++ grade/report/user/lang/en/gradereport_user.php
@@ -22,5 +22,5 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'User report';
+$string['pluginname'] = 'Teacher Report';
 $string['user:view'] = 'View your own grade report';
