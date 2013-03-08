38c4e797147f959dfae398872686a6f904f2c9cd
Adam Morris <amorris@mistermorris.com>
2013-03-08 12:52:13 +0800

    changed my mind about putting default settings in url module

--- mod/url/mod_form.php
+++ mod/url/mod_form.php
@@ -70,9 +70,7 @@ class mod_url_mod_form extends moodleform_mod {
             $mform->setDefault('display', key($options));
         } else {
             $mform->addElement('select', 'display', get_string('displayselect', 'url'), $options);
-	    //ssis default should be to open a url, change default:
-            //$mform->setDefault('display', $config->display);
-	    $mform->setDefault('display', RESOURCELIB_DISPLAY_OPEN);
+            $mform->setDefault('display', $config->display);
             $mform->setAdvanced('display', $config->display_adv);
             $mform->addHelpButton('display', 'displayselect', 'url');
         }
