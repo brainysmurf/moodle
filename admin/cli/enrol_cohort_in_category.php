702c9ae82938bac692f79cf51959d53f48a1c4f1
Anthony Kuske <anthonykuske@gmail.com>
2013-11-08 14:13:28 +0800

    Script to enrol a cohort in all courses in a category

---
+++ admin/cli/enrol_cohort_in_category.php
@@ -0,0 +1,161 @@
+<?php
+
+/*
+
+	Usage:
+	cd /admin/cli
+	php enrol_cohort_in_category.php cohortIDNumber courseID roleName
+	
+	e.g.
+	php enrol_cohort_in_category.php cohortIDNumber courseID roleName
+	php enrol_cohort_in_category.php studentsSEC 87 Teacher
+	
+*/
+
+define('CLI_SCRIPT', true);
+
+require(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once($CFG->libdir.'/clilib.php'); 
+require_once($CFG->dirroot.'/cohort/lib.php');
+require_once($CFG->libdir.'/coursecatlib.php');
+require_once('../../enrol/locallib.php');
+
+	
+	//Chort IDnumber
+	$cohortID = $argv[1];
+	$cohort = $DB->get_record('cohort',array('idnumber'=>$cohortID));
+	
+	if ( !$cohort )
+	{
+		die("\nCohort not found.\n");
+	}
+	
+	echo "\nCohort: ".$cohort->name." (".$cohort->idnumber.")";
+	
+	//Get members of cohort
+	$user_ids = array();
+	$cohort_members = $DB->get_records('cohort_members',array('cohortid'=>$cohort->id));
+	foreach ( $cohort_members as $member )
+	{
+		$user_ids[] = $member->userid;
+	}
+	
+	if ( count($user_ids) < 1 )
+	{
+		die("\nThere are no members of this cohort.\n");
+	}
+	else
+	{
+		echo "\n".number_format(count($user_ids))." users in this cohort.";
+	}
+	
+	//Category ID
+	$categoryID = $argv[2];
+	$category = coursecat::get($categoryID);
+	
+	if ( !$category )
+	{
+		die("\nCategory not found.\n");
+	}
+	
+	echo "\nCategory: ".$category->name;
+	
+	//Get all courses in category and subcategory
+	$courses = $category->get_courses(array('recursive'=>true));
+	
+	echo "\nCourses...";
+	
+	$course_ids = array();
+	foreach ( $courses as $course )
+	{
+		echo "\n\t".$course->fullname;
+		$course_ids[] = $course->id;
+	}
+	
+	if ( count($course_ids) < 1 )
+	{
+		die("\nThere are no courses in this category or its subcategories.\n");
+	}
+	else
+	{
+		echo "\n".number_format(count($course_ids))." courses in this category (including those in subcategories).";
+	}
+	
+	// Role to give the users
+	$roleName = $argv[3];
+	
+	$role = $DB->get_record( 'role', array('name'=>$roleName));
+	
+	if ( !$role )
+	{
+		die("\nRole not found\n");
+	}
+	else
+	{
+		echo "\nRole: ".$role->name." (".$role->id.")";
+		$roleid = $role->id;
+	}
+	
+	echo "\n\nAll users in to cohort ".$cohort->name." (".$cohort->idnumber.") will be enrolled in the courses shown.\n";
+	
+	$response = cli_input("Enter Y to continue.");
+	
+	if ( trim(strtoupper($response)) !== 'Y' ) { die(); }
+	
+	
+	//Go time
+	
+	foreach( $course_ids as $courseid )
+	{
+		echo "\nCourse $courseid...";
+		
+		//Get full course data from DB
+		$course = $DB->get_record('course',array('id'=>$courseid));
+		
+		//Get context
+		if ( !$context = get_context_instance(CONTEXT_COURSE, $courseid, MUST_EXIST) )
+		{
+			die("\nCould not get context for course ".$courseid."\n");
+		}
+		
+		//Create an enrolment manager for the course
+		$manager = new course_enrolment_manager($PAGE, $course);
+		
+		//Get the enrolment method ID for the manual enrolment method for this course
+		$enrolMethod = $DB->get_record('enrol', array('enrol'=>'manual', 'courseid'=>$courseid), '*', MUST_EXIST);
+		if ( !$enrolMethod )
+		{
+			die("\nNo manual enrolment method found for course $courseid\n");
+		}
+		
+		$enrolMethodID = $enrolMethod->id;
+		
+		//Create a manual enrolment plugin object
+		$plugins = $manager->get_enrolment_plugins();
+	  	$plugin = $plugins['manual'];
+	  	
+	  	$instances = $manager->get_enrolment_instances();
+		$instance = $instances[$enrolMethodID];
+		
+		foreach( $user_ids as $userid )
+		{
+			if ( is_enrolled($context, $userid) )
+			{
+				//This would break if the user is already enrolled but with the wrong role
+				echo "\nUser $userid is already enrolled in course $courseid";			
+				continue;
+			}
+			
+			echo "\nEnrolling user $userid in course $courseid";
+			
+			$today = time();
+			$timestart = make_timestamp(date('Y', $today), date('m', $today), date('d', $today), 0, 0, 0);
+			$timeend = 0;
+
+			$plugin->enrol_user($instance, $userid, $roleid, $timestart, $timeend);
+		}
+	}
+	
+	echo "\n";
+
+?>
