a7f9b99920ac7eb0144441996816409a39c72ff2
Anthony Kuske <anthonykuske@gmail.com>
2013-11-11 13:21:18 +0800

    Removed leading ': ' from page titles

--- theme/decaf/layout/general.php
+++ theme/decaf/layout/general.php
@@ -72,7 +72,7 @@ if (check_browser_version("MSIE", "0")) {
 echo $OUTPUT->doctype() ?>
 <html <?php echo $OUTPUT->htmlattributes() ?>>
 <head>
-    <title><?php echo $PAGE->title ?></title>
+    <title><?php echo ltrim($PAGE->title,': ') ?></title>
     <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>" />
     <?php echo $OUTPUT->standard_head_html() ?>
 </head>
