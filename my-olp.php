196752e2aab80797ff5ec9e4cd9380075b18170d
Anthony Kuske <anthonykuske@gmail.com>
2013-12-02 14:30:30 +0800

    /my-olp.php redirect to user's online portfolio

---
+++ my-olp.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+	This page lists all the icons available in fontawesome
+*/
+require_once(dirname(__FILE__) . '/config.php');
+require_once($CFG->basedir . 'course/lib.php');
+
+if ($olpCourseID = get_olp_courseid($USER->idnumber)) {
+	redirect('/course/view.php?id=' . $olpCourseID);
+}
+
+$PAGE->set_url('/my-olp.php');
+$PAGE->set_title('My Online Portfolio');
+$PAGE->set_heading('My Online Portfolio');
+
+require_login();
+
+echo $OUTPUT->header();
+
+	echo $OUTPUT->errorbox('Sorry, we couldn\'t find an online portfolio course for you on DragonNet');
+
+echo $OUTPUT->footer();
