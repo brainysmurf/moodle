4bdfe4d7f6505c3d447336497df8884d9e764b4f
Anthony Kuske <anthonykuske@gmail.com>
2014-02-24 11:08:16 +0800

    favicon and font-awesome links in head are relative

--- theme/decaf/layout/general.php
+++ theme/decaf/layout/general.php
@@ -73,8 +73,9 @@ echo $OUTPUT->doctype() ?>
 <html <?php echo $OUTPUT->htmlattributes() ?>>
 <head>
 	<title><?php echo ltrim($PAGE->title,': ') ?></title>
-	<link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>" />
+	<link rel="shortcut icon" href="/favicon.ico" />
 	<?php echo $OUTPUT->standard_head_html() ?>
+	<link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css" />
 </head>
 
 <body id="<?php p($PAGE->bodyid) ?>" class="<?php p($PAGE->bodyclasses.' '.join(' ', $bodyclasses)) ?>">
