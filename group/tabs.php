220a32b52b50a3b7cb7b4d670a0664580506a3c2
Anthony Kuske <anthonykuske@gmail.com>
2014-02-24 11:42:55 +0800

    Fix group buttons

--- group/tabs.php
+++ group/tabs.php
@@ -22,17 +22,17 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
     $row = array();
-    $row[] = new tabobject('groups',
+    $row[] = new tabobject('tab-groups',
                            new moodle_url('/group/index.php', array('id' => $courseid)),
                            get_string('groups'));
 
-    $row[] = new tabobject('groupings',
+    $row[] = new tabobject('tab-groupings',
                            new moodle_url('/group/groupings.php', array('id' => $courseid)),
                            get_string('groupings', 'group'));
 
-    $row[] = new tabobject('overview',
+    $row[] = new tabobject('tab-overview',
                            new moodle_url('/group/overview.php', array('id' => $courseid)),
                            get_string('overview', 'group'));
     echo '<div class="groupdisplay">';
-    echo $OUTPUT->tabtree($row, $currenttab);
+    echo $OUTPUT->tabtree($row, "tab-{$currenttab}");
     echo '</div>';
