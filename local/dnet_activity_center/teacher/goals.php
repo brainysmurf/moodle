44b42754de86af4b511029b28db3b0986ded2e9a
Adam Morris <amorris@mistermorris.com>
2014-06-05 22:11:58 +0800

    highlight goal tab

--- local/dnet_activity_center/teacher/goals.php
+++ local/dnet_activity_center/teacher/goals.php
@@ -2,7 +2,7 @@
 
 include '../roles/common_top.php';
 
-echo $activityCenter->display->showTabs('teacher', 'goal');
+echo $activityCenter->display->showTabs('teacher', 'goals');
 echo $OUTPUT->sign('plus-sign', 'Enter your goals', 'Explanation');
 
 $goal = $activityCenter->data->getUserGoal($activityCenter->userid());
