2996233ce723a14de80d9f2f18a4e4b1c8988424
Anthony Kuske <anthonykuske@gmail.com>
2014-06-09 11:06:38 +0800

    Activity supervisors field four courses

--- course/edit_form.php
+++ course/edit_form.php
@@ -308,10 +308,10 @@ class course_edit_form extends moodleform {
 	$mform->addElement('text', 'ssismetadata[grade2]', 'Grade 2', 'maxlength="100" size="20"');
 	$mform->setType('ssismetadata[grade2]', PARAM_INT);
 
-	$mform->addElement('text', 'ssismetadata[activityseason]', 'Activities: Season', 'maxlength="100" size="20"');
+	$mform->addElement('text', 'ssismetadata[activityseason]', 'Activity Season', 'maxlength="100" size="20"');
 	$mform->setType('ssismetadata[activityseason]', PARAM_TEXT);
 
-	$mform->addElement('text', 'ssismetadata[activitysupervisors]', 'Activities: Supervisors Required', 'maxlength="100" size="20"');
+	$mform->addElement('text', 'ssismetadata[activitysupervisors]', 'Activity Supervisors Required', 'maxlength="100" size="20"');
 	$mform->setType('ssismetadata[activitysupervisors]', PARAM_INT);
 
 //--------------------------------------------------------------------------------
