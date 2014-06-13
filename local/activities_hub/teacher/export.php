fb7422cd08fe53203e016f78a56ab5104b4b2b30
Adam Morris <amorris@mistermorris.com>
2014-06-13 15:37:10 +0800

    fix for export persmission check

--- local/activities_hub/teacher/export.php
+++ local/activities_hub/teacher/export.php
@@ -8,7 +8,7 @@ require '../../../config.php';
 
 require_login();
 
-if (!has_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM)) && empty($SESSION->userIsSecretary)) {
+if (!is_admin() or !is_activities_head()) {
 	die("Only admins may do that.");
 }
 
