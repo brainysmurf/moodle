5ebe161e7a03a4e503d32d1f26360abba7162658
Anthony Kuske <anthonykuske@gmail.com>
2015-03-13 15:59:49 +0000

    remove user from deploy script

--- pull-staging.php
+++ pull-staging.php
@@ -1,11 +1,8 @@
 <?php
 require 'vendor/autoload.php';
 $deployer = new \Tmd\AutoGitPull\Deployer(array(
-    'deployUser' => 'www-data',
     'directory' => '/var/www/dragonnet/moodle/',
     'logDirectory' => '/var/log/moodlepull/',
-    'notifyEmails' => array(
-    ),
     'branch' => 'staging'
 ));
 $deployer->postDeployCallback = function () {
